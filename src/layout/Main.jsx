8ef1c8be2c6601625eb7e4c475cf52e7a58f326a
Yauheniya Sirotkina <zsirotkina@gmail.com>
2021-12-29 01:07:52 +0300

    update Main component

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 
 class Main extends React.Component {
     state = {
         movies: [],
-        isLoaded: false
+        isLoaded: false,
     }
 
     componentDidMount() {
@@ -19,12 +20,23 @@ class Main extends React.Component {
         });
     }
 
+    searchMovies = (searchStatment) => {
+        this.setState({ isLoaded: false });
+        fetch(`http://www.omdbapi.com/?apikey=[YOUR_APIKEY]&r=json&s=${searchStatment}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.Response === 'True') {
+                this.setState({ movies: data.Search, isLoaded: true });
+            }
+        });
+    }
+
     render() {
         const { movies, isLoaded } = this.state;
-        console.dir(movies);
 
         return (
             <main className="container content">
+                <Search searchMovies={this.searchMovies} />
                 {isLoaded ? (
                     <Movies movies={movies} />
                 ) : (
