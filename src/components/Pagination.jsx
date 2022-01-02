279eb9fb52c01a716eec55df006f28f00fe8c931
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-02 18:36:52 +0300

    fix pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -13,8 +13,8 @@ class Pagination extends React.Component {
             
             if (currentPage === 1) {
                 this.setState({ currentPage, startPage: 1, endPage: 5 })
-            } else if (currentPage === this.props.pages) {
-                this.setState({ currentPage, startPage: this.props.pages - 5, endPage: this.props.pages })
+            } else if (currentPage === this.props.totalPages) {
+                this.setState({ currentPage, startPage: this.props.totalPages - 5, endPage: this.props.totalPages })
             } else {
                 this.setState({ currentPage, startPage: currentPage, endPage: currentPage + 5 })
             }
@@ -29,8 +29,8 @@ class Pagination extends React.Component {
 
         if(currentPage === 1) {
             this.setState({ startPage: 1, endPage: 5 })
-        } else if (currentPage === this.props.pages) {
-            this.setState({ startPage: this.props.pages - 5, endPage: this.props.pages })
+        } else if (currentPage === this.props.totalPages) {
+            this.setState({ startPage: this.props.totalPages - 5, endPage: this.props.totalPages })
         }
     }
 
@@ -52,16 +52,16 @@ class Pagination extends React.Component {
         this.props.changePage(currentPage);
 
         const startPage = currentPage;
-        const endPage = startPage + 5 <= this.props.pages ? startPage + 5 : this.props.pages;
+        const endPage = startPage + 5 <= this.props.totalPages ? startPage + 5 : this.props.totalPages;
         this.setState({ startPage, endPage })
     }
 
     increasePage = () => {
-        if (this.state.currentPage !== this.props.pages) {
+        if (this.state.currentPage !== this.props.totalPages) {
             const currentPage = this.state.currentPage + 1;
             if (this.state.currentPage === this.state.endPage) {
                 const startPage = currentPage;
-                const endPage = startPage + 5 <= this.props.pages ? startPage + 5 : this.props.pages;
+                const endPage = startPage + 5 <= this.props.totalPages ? startPage + 5 : this.props.totalPages;
                 this.setState({ currentPage, startPage, endPage })
             } else {
                 this.setState({ currentPage })
@@ -88,7 +88,7 @@ class Pagination extends React.Component {
 
     render () {
         const { currentPage, startPage, endPage } = this.state;
-        const { pages } = this.props;
+        const { totalPages } = this.props;
    
         const lis = [];
         if (startPage !== 1) {
@@ -101,15 +101,15 @@ class Pagination extends React.Component {
             lis.push(<li className={i === currentPage ? "active" : "waves-effect"}><a href="#" onClick={this.selectPage}>{i}</a></li>);
         }
         lis.push(<li className={endPage === currentPage ? "active" : "waves-effect"}><a href="#" onClick={this.selectLastPageInRange}>{endPage}</a></li>);
-        if (endPage !== pages) {
+        if (endPage !== totalPages && endPage < totalPages) {
             lis.push(<li>...</li>);
-            lis.push(<li className={pages === currentPage ? "active" : "waves-effect"}><a href="#" onClick={this.selectPage}>{pages}</a></li>);
+            lis.push(<li className={totalPages === currentPage ? "active" : "waves-effect"}><a href="#" onClick={this.selectPage}>{totalPages}</a></li>);
         }
 
         return (
             <>
                 {
-                    pages ? (
+                    totalPages && totalPages !== 1 ? (
                         <div className="pagination-wrapper">
                             <ul className="pagination">
                                 <li className={currentPage === 1 ? "disabled" : "waves-effect"}>
@@ -118,7 +118,7 @@ class Pagination extends React.Component {
                                     </a>
                                 </li>
                                     {lis}
-                                <li className={currentPage === pages ? "disabled" : "waves-effect"}>
+                                <li className={currentPage === totalPages ? "disabled" : "waves-effect"}>
                                     <a href="#" onClick={this.increasePage}>
                                         <i className="material-icons">chevron_right</i>
                                     </a>
