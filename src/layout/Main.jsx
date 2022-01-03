73e00af2187a07d8c1ee519561b0c65bcd7077c9
Yauheniya Sirotkina <zsirotkina@gmail.com>
2022-01-03 13:31:58 +0300

    remove pagination

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,6 @@ import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 import { Filter } from '../components/Filter';
-import { Pagination } from '../components/Pagination';
 import { NotFound } from '../components/NotFound';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -14,22 +13,16 @@ class Main extends React.Component {
         isLoaded: false,
         errorMessage: undefined,
         searchStatment: 'home',
-        page: 1,
-        totalPages: undefined,
         type: ''
     }
 
-    generateRequest = (searchStatment, page, type) => {
+    generateRequest = (searchStatment, type) => {
         let request = `https://www.omdbapi.com/?apikey=${API_KEY}&r=json`;
 
         if (searchStatment) {
             request += `&s=${searchStatment}`;
         } else {
             request += '&s=home';
-        }
-
-        if (page) {
-            request += `&page=${page}`;
         }
 
         if (type && type !== 'all') {
@@ -44,72 +37,65 @@ class Main extends React.Component {
         .then(response => response.json())
         .then(data => {
             if (data.Response === 'True') {
-                const totalPages = Math.ceil(data.totalResults / 10);
-                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true, totalPages });
+                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true });
             }
             if (data.Response === 'False') {
-                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false, totalPages: undefined });
+                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false });
             }
         });
     }
 
     searchMovies = (searchStatment) => {
-        this.setState({ searchStatment, isLoaded: false, page: 1 });
-        fetch(this.generateRequest(searchStatment, this.state.page, this.state.type))
+        this.setState({ searchStatment, isLoaded: false});
+        fetch(this.generateRequest(searchStatment, this.state.type))
         .then(response => response.json())
         .then(data => {
             if (data.Response === 'True') {
-                const totalPages = Math.ceil(data.totalResults / 10);
-                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true, totalPages });
+                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true });
             }
             if (data.Response === 'False') {
-                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false, totalPages: undefined });
+                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false });
             }
         });
     }
 
     filterMovies = (filter) => {
-        const currentPage = 1;
-        this.setState({ isLoaded: false, page: currentPage, type: filter });
+        this.setState({ isLoaded: false, type: filter });
 
-        fetch(this.generateRequest(this.state.searchStatment, currentPage, filter))
+        fetch(this.generateRequest(this.state.searchStatment, filter))
         .then(response => response.json())
         .then(data => {
             if (data.Response === 'True') {
-                const totalPages = Math.ceil(data.totalResults / 10);
-                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true, totalPages });
+                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true });
             }
             if (data.Response === 'False') {
-                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false, totalPages: undefined });
+                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false });
             }
         });
     }
 
     changePage = (page) => {
-        this.setState({ isLoaded: false, page: page });
-        fetch(this.generateRequest(this.state.searchStatment, page, this.state.type))
+        this.setState({ isLoaded: false });
+        fetch(this.generateRequest(this.state.searchStatment, this.state.type))
         .then(response => response.json())
         .then(data => {
             if (data.Response === 'True') {
-                const totalPages = Math.ceil(data.totalResults / 10);
-                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true, totalPages });
+                this.setState({ movies: data.Search, errorMessage: undefined, isLoaded: true });
             }
             if (data.Response === 'False') {
-                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false, totalPages: undefined });
+                this.setState({ movies: [], errorMessage: data.Error, isLoaded: false });
             }
         });
     }
 
     render() {
-        const { movies, isLoaded, errorMessage, page, totalPages } = this.state;
+        const { movies, isLoaded, errorMessage } = this.state;
 
         return (
             <main className="container content">
                 <Search searchMovies={this.searchMovies} />
                 <Filter filterMovies={this.filterMovies} />
-   
-                {totalPages && <Pagination currentPage={page} totalPages={totalPages} changePage={this.changePage} />}
-                     
+
                 {!isLoaded && errorMessage ? (
                     <NotFound message={errorMessage} />
                 ) : isLoaded && !errorMessage ? (
